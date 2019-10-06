b0ad3d2169446c4bf8e56b61997b5292ce216906
Lukasz Farys <lukaszfarys@gmail.com>
2019-10-06 21:40:09 +0200

    edit database model

--- Arbitraz/src/main/java/pl/coderslab/app/web/controllers/HomePageController.java
+++ Arbitraz/src/main/java/pl/coderslab/app/web/controllers/HomePageController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.coderslab.app.services.ExchangeService;
 
 import java.security.Principal;
 import java.time.LocalDateTime;
@@ -12,11 +13,19 @@ import java.time.LocalDateTime;
 @Controller
 @RequestMapping("/")
 public class HomePageController {
+    private ExchangeService exchangeService;
+
+    public HomePageController(ExchangeService exchangeService) {
+        this.exchangeService = exchangeService;
+    }
+
     @GetMapping
     public String prepareHomePage(Model model, Principal principal) {
         String username = principal.getName();
         model.addAttribute("username", username);
         String time = LocalDateTime.now().toString();
+        System.out.println(exchangeService.getExchangeById(1L).getAddressUrlPrefix());
+
         return "index";
     }
 }
